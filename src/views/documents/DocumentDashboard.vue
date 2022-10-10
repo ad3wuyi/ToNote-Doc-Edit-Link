ecaf2346c14e9b71d33bfa82cab0f076a4b51a25
Demola <64336052+ekogeda@users.noreply.github.com>
2022-10-10 20:53:51 +0100

    Updated

--- src/views/documents/DocumentDashboard.vue
+++ src/views/documents/DocumentDashboard.vue
@@ -3,7 +3,8 @@
     <h1 class="text-center mb-3">Welcome to ToNote Link Creation</h1>
 
     <ul>
-      <li>Copy a link having the document ID inclusive e.g.
+      <li>
+        Copy a link having a document ID inclusive e.g.
         <ul>
           <li>
             <a href="https://tonote-doc-link.netlify.app/document/edit/f80e326b-5d4b-427e-ab39-280126aa1acd"
@@ -13,5 +14,15 @@
         </ul>
       </li>
     </ul>
+
+    <div class="my-2"></div>
+
+    <h1 class="text-center mb-2">OR</h1>
+
+    <p class="text-center">
+      <router-link :to="{ name: 'document.upload' }" class="btn btn-sm btn-primary">
+        Upload a document
+      </router-link>
+    </p>
   </div>
 </template>
