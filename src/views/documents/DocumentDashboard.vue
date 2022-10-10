1397cb934305ca7a489fa2acb0f263b12f1bbf3e
Demola <64336052+ekogeda@users.noreply.github.com>
2022-10-10 18:10:55 +0100

    Redirect

--- src/views/documents/DocumentDashboard.vue
+++ src/views/documents/DocumentDashboard.vue
@@ -1,5 +1,15 @@
 <template>
   <div>
     <h1>Welcome to ToNote Document Editing Dashboard</h1>
+    <UserVerify />
   </div>
 </template>
+
+<script setup>
+import UserVerify from '@/components/Auth/UserVerify.vue';
+
+</script>
+
+<style scoped>
+
+</style>
