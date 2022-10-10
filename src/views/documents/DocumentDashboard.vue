ee048e6c530c841064e77b63a4cb99d841da3faa
Demola <64336052+ekogeda@users.noreply.github.com>
2022-10-10 20:29:46 +0100

    Redirect

--- src/views/documents/DocumentDashboard.vue
+++ src/views/documents/DocumentDashboard.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="my-5 py-5">
-    <h1 class="text-center">Welcome to ToNote Link Creation</h1>
+  <div class="my-5 py-5 w-50 mx-auto">
+    <h1 class="text-center mb-3">Welcome to ToNote Link Creation</h1>
 
     <ul>
-      <li>Copy a link having the document id inclusive
+      <li>Copy a link having the document ID inclusive e.g.
         <ul>
           <li>
             <a href="https://tonote-doc-link.netlify.app/document/edit/f80e326b-5d4b-427e-ab39-280126aa1acd"
