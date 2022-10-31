81ce92cf4141d36870bff24f8f87884101fdebe3
Demola <64336052+ekogeda@users.noreply.github.com>
2022-10-31 09:28:10 +0100

    Redirecting to website when logout

--- src/components/Document/Edit/Left/AsideLeft.vue
+++ src/components/Document/Edit/Left/AsideLeft.vue
@@ -27,6 +27,16 @@
                     </button>
                   </div>
                 </div>
+                <div class="list-group-item border-bottom">
+                  Tool Management
+                </div>
+                <div v-show="!hasRole" class="text-center mt-5">
+                  <span class="spinner-border spinner-border-sm"></span>
+                  Loading...
+                </div>
+                <div v-if="hasRole">
+                  <ToolKits @bindTool="addMouseMoveListener" />
+                </div>
               </template>
               <template v-else>
                 <div class="list-group-item border-bottom">
@@ -36,16 +46,6 @@
                   </p>
                 </div>
               </template>
-              <div class="list-group-item border-bottom">
-                Tool Management
-              </div>
-              <div v-show="!hasRole" class="text-center mt-5">
-                <span class="spinner-border spinner-border-sm"></span>
-                Loading...
-              </div>
-              <div v-if="hasRole">
-                <ToolKits @bindTool="addMouseMoveListener" />
-              </div>
             </div>
           </div>
         </div>
